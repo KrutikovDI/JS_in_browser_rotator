6e24034945431374a2c49e376c1243d63b73135c
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-09 14:27:53 +0700

    Correction JS

--- task.js
+++ task.js
@@ -1,10 +1,15 @@
-setInterval(() => {
-  let rot = document.querySelector(".rotator__case.rotator__case_active")
-  rot.classList.remove("rotator__case_active")
-  if (rot.nextElementSibling != null) {
-    rot.nextElementSibling.classList.add("rotator__case_active")
-    rot.style.color = rot.getAttribute("data-color")
+function f() {
+  let el = document.querySelector(".rotator__case.rotator__case_active")
+  el.classList.remove("rotator__case_active")
+  elNext = el.nextElementSibling
+  el.style.color = el.getAttribute("data-color")
+  if (elNext != null) {
+    elNext.classList.add("rotator__case_active")
   }
   else {
-    document.querySelector(".rotator").firstElementChild.classList.add("rotator__case_active")}
-}, document.querySelector(".rotator__case.rotator__case_active").getAttribute("data-speed"))
+    document.querySelector(".rotator").firstElementChild.classList.add("rotator__case_active")
+  }
+  return setTimeout(() => {f()}, document.querySelector(".rotator__case.rotator__case_active").getAttribute("data-speed"))
+}
+
+f()
